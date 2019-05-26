6b1f7e9372189f67d9f19bf936afea91f44e073f
JohnWink <luisbpires@hotmail.com>
2019-05-26 22:12:34 +0100

    Javascript update
    On usersModel, usersController and LandingPageView

--- JS/Controllers/usersController.js
+++ JS/Controllers/usersController.js
@@ -1,2 +1,50 @@
 import User from "../Models/usersModel";
 
+//Defines an array to save user Objects
+export let users=[]
+
+// In case a user key exists in LocalStorage, this one is loaded into the array
+//Otherwise, they are saved into the array with several objects User are inserted manualy
+if(localStorage.users){
+    users = JSON.parse(localStorage.users)
+}else{
+    const user1= new User("JohnWink", "12345")
+    const user2= new User("Mint", "54321")
+    const user3= new User("Tiago", "15234")
+    users.push(user1,user2,user3);
+    localStorage.setItem("users", JSON.stringify(users))
+}
+
+//Function that checks a user (update of the key on the session storage), returns "true" if credentials exist.
+export function login(usernameLogin, passwordLogin){
+    let existUser = false
+    for(const user of users){
+        if(user.username === usernameLogin && user.password == passwordLogin){
+            sessionStorage.setItem("loggedUser", usernameLogin)
+            existUser = true
+        }
+    }
+    return existUser
+}
+
+//Logout function
+export function logout(){
+    sessionStorage.removeItem("loggedUser")
+}
+
+// Function that registers a user(update of the key in SessionStorage)
+export function register(usernameRegister, passwordRegister){
+    let existUser=false
+    for(const user of users){
+        if(user.username === usernameRegister && user.password === passwordRegister){
+            existUser= true
+        }
+    }
+    if(!existUser){
+        users.push(new User(usernameRegister, passwordRegister))
+        localStorage.setItem("users", JSON.stringify(users))
+        sessionStorage.setItem("loggedUser", usernameRegister)
+        return true
+    }
+    return false
+}
---
+++ JS/Views/LandingPageView.js
@@ -0,0 +1,29 @@
+import{
+    login,
+    logout,
+    register
+}from "../Controllers/usersController"
+
+document.querySelector("#frmSignUp").addEventListener("submit", function (event) {
+
+    const pass = document.querySelector("#passwordRegister").value
+    const confirmPass = document.querySelector("#passwordRegisterConfirm").value
+
+    if(pass!==confirmPass){
+        alert("As passwords Têm de ser iguais")
+    }else{
+        const registerResult = register(
+            document.querySelector("#usernameRegister").value,
+            document.queryCommandEnabled("#passwordRegister").value)
+        if(registerResult == true){
+            //Close modal window
+            $('#signupModal').modal('hide')
+            //Present the authentication of the user
+            alert(`Olá ${sessionStorage.getItem("loggedUser")}`)
+        }else{
+            alert("Credenciais já existentes")
+        }
+    }
+    //Prevenção da submissão do formulário
+    event.preventDefault()
+})
