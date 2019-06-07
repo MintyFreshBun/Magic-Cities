a08f0cc4deda85220e0f0d67d9d7a95479b74aec
Luis Pires <luisbpires@hotmail.com>
2019-06-07 15:31:02 +0100

    adding "change avatar button"

--- JS/Controllers/usersController.js
+++ JS/Controllers/usersController.js
@@ -8,9 +8,9 @@ export let users=[]
 if(localStorage.users){
     users = JSON.parse(localStorage.users)
 }else{
-    const user1= new User("JohnWink", "12345", "0","1","100")
-    const user2= new User("Mint", "54321","0","1","100")
-    const user3= new User("Tiago", "15234","0","1","100")
+    const user1= new User("JohnWink", "12345", "0","1","0")
+    const user2= new User("Mint", "54321","0","1","0")
+    const user3= new User("Tiago", "15234","0","1","0")
     users.push(user1,user2,user3);
     localStorage.setItem("users", JSON.stringify(users))
 }
