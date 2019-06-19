2ee8639af3d5287987105601220a6189ea23f06a
Miguel Amaral <mikefirebun@gmail.com>
2019-06-19 12:17:13 +0100

    fixed some stuff on the admin side and removing city is done and working

--- JS/Views/CatalgPageView.js
+++ JS/Views/CatalgPageView.js
@@ -54,7 +54,9 @@ function renderCatalog (filterName = "", filterZone = ""){
 
                     <div class="row" style="padding-top: 2%">
                         <div class="col"><button type="button" id="${city.name}" class="btn btn-primary view"> Visitar Pagina</button></div>`
-                        if(sessionStorage.getItem("admin") == true){ // if the user is an admin thne the remove button will show
+                        if(sessionStorage.getItem("admin")){ 
+                            console.log("if entered")
+                            // if the user is an admin thne the remove button will show
                             result+= `<div class="col"><button type="button" id="${city.name}" class="btn btn-danger remove"> Remover</button></div>`
                         }
                                             
@@ -138,7 +140,7 @@ function renderCatalog (filterName = "", filterZone = ""){
 }
 
 // adding the add city button if admin == true and its fuctions
-if(sessionStorage.getItem("admin") == true){
+if(sessionStorage.getItem("admin")){
     document.querySelector(".form-Class").innerHTML += `<button type="button" class="btn btn-primary m-2">
     Adicionar Cidade
     </button>`
@@ -146,7 +148,7 @@ if(sessionStorage.getItem("admin") == true){
 }
 
 document.querySelector(".form-Class").addEventListener("click", function () {
-    location.href = "AddCityPage.html"
+    location.href = "AddCity.html"
 })
 
 
